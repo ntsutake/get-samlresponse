f1c434b3919d34304fc4c8355fc756f81aff20a2
ntsutake <naoki.tsutake@sbisecsol.com>
2021-01-13 14:55:59 +0900

    add download button

--- popup.js
+++ popup.js
@@ -1,3 +1,13 @@
 var textContents = chrome.extension.getBackgroundPage().textContents;
 var div = document.getElementById('textView');
 div.textContent = textContents;
+
+download.addEventListener("click", function() {
+    var a = document.createElement('a');
+    var text = document.getElementById("textView").value;
+    a.href = 'data:text/plain;charset=utf-8,'
+        + encodeURIComponent(text);
+    a.setAttribute('download', 'saml.txt');
+    // a.dispatchEvent(new CustomEvent('click'));
+    a.click();
+});
